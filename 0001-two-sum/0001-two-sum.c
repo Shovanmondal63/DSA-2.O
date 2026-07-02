d5e8eeb8255d9864c98d8e960465bd9a7c73386a
shovan mondal <shovanmmondalmm@gmail.com>
2026-07-02 21:32:33 +0530

    Time: 99 ms (79.7%), Space: 9.4 MB (42.7%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.c
@@ -0,0 +1,21 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int i,j,index1,index2;
+    for(i=0;i<numsSize;i++)
+    {
+        for(j=i+1;j<numsSize;j++)
+        {
+            if((nums[i]+nums[j]) == target)
+            {
+                nums[0]=i;
+                nums[1]=j;
+                *returnSize = 2;
+                return nums;
+                break;
+            }
+        }
+    }
+    return 0;
+}
